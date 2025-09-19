395016587759049a1d210a3948bcaffd0a6fcdbe
pradanaadn <pradanaadn21@gmail.com>
2025-09-19 18:00:02 +0800

    feat: add Role and Users tables with relationships in the database schema

---
+++ src/db/Role.ts
@@ -0,0 +1,12 @@
+import { uuid, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const Roles = pgTable("roles", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull().unique(),
+  description: varchar("description", { length: 512 }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
