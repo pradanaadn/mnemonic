447225859ba4fb09c7f4abb0527c37ac7a87b75d
pradanaadn <pradanaadn21@gmail.com>
2025-09-22 21:57:31 +0800

    fix: remove duplicate export of Users schema in index file

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,5 @@ export * from './schemas/Notes';
 export * from './schemas/Tags';
 export * from './schemas/Users';
 export * from './schemas/Role';
-export * from './schemas/Users';
 export * from './schemas/VisibilityEnum';
 export * from './connections';
